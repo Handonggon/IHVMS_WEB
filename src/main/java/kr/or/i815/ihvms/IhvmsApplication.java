ffa229a4823a0e4466d45c832f3fcbcf95a48abc
Handonggon <gksehdrhs2@gmail.com>
2022-09-25 20:42:26 +0900

    init

---
+++ src/main/java/kr/or/i815/ihvms/IhvmsApplication.java
@@ -0,0 +1,13 @@
+package kr.or.i815.ihvms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IhvmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IhvmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/or/i815/ihvms/IhvmsApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.or.i815.ihvms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IhvmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
